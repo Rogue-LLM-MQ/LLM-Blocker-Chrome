7defcc4e3b7d76e494528bdbd3ad83c9d05e22dc
Adrian Guereque <aguereque96@gmail.com>
2025-09-16 23:41:51 +1000

    Feat: Add changed popup pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import EventDashboard from './EventDashboard.jsx'
+import LLM_Checker from './LLM_Checker.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <EventDashboard />
+    <LLM_Checker/>
   </StrictMode>,
 )
